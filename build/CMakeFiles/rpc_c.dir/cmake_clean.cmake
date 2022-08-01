47c35c02272782ea39b914f5ef9f76d41f6413c6
xxyzll <xxyzll@yeah.net>
2022-08-01 20:30:53 +0800

    init

---
+++ build/CMakeFiles/rpc_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rpc_c.dir/main.cpp.o"
+  "rpc_c.pdb"
+  "rpc_c"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rpc_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
